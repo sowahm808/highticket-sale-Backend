cc024a12801f05d493809a01c3394b20ca651497
Michael Sowah <sowahm808@gmail.com>
2025-08-12 18:46:35 -0500

    initial commit

---
+++ tsconfig.ts
@@ -0,0 +1,15 @@
+{
+  "compilerOptions": {
+    "module": "esnext",
+    "target": "es2021",
+    "moduleResolution": "node",
+    "outDir": "dist",
+    "rootDir": "src",
+    "experimentalDecorators": true,
+    "emitDecoratorMetadata": true,
+    "strict": true,
+    "skipLibCheck": true,
+    "esModuleInterop": true
+  },
+  "include": ["src/**/*"]
+}
