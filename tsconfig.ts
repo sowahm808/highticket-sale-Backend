70af5af7f156b96984cd2ecf7ddecd5dbd504eea
Michael Sowah <sowahm808@gmail.com>
2025-08-12 21:48:18 -0500

    updated

---
+++ tsconfig.ts
@@ -0,0 +1,16 @@
+{
+  "compilerOptions": {
+    "module": "esnext",
+    "target": "es2021",
+    "moduleResolution": "node",
+    "outDir": "dist",
+    "rootDir": "src",
+    "experimentalDecorators": true,
+    "emitDecoratorMetadata": true,
+    "strict": true,
+    "skipLibCheck": true,
+    "types": ["node"],
+    "esModuleInterop": true
+  },
+  "include": ["src/**/*"]
+}
